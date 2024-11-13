dc7995bab4f30e797c637fe2f55f8bc1b02a7589
Moustafa Shaaban <Moustafa_Shaaban@zohomail.com>
2024-11-13 22:11:29 +0200

    Updated HomeView to ist all expenses

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,14 +4,42 @@ import { useExpensesStore } from '@/stores/expenses';
 const expensesStore = useExpensesStore();
 
 const totalAmount = expensesStore.totalAmount;
+
+const expenses = expensesStore.expenses;
+
+function onClick () {
+        // console.log('Clicked on a QChip')
+}
 </script>
 
 <template>
-  <h1>Home page</h1>
-  <p>You have: $ {{ totalAmount }}</p>
+  <q-page>
+    <q-list>
+      <q-item v-for="expense in expenses" :key="expense.id">
+        <q-item-section>
+          <q-item-label>
+            <div class="q-pa-md">
+              {{ expense.name }}
+              <q-chip v-for="tag in expense.tags" :key="tag.id" size="sm" clickable @click="onClick" color="primary" text-color="white" icon="event">
+                {{ tag.name }}
+              </q-chip>
+            </div>
+        </q-item-label>
+        </q-item-section>
 
-  <q-page-sticky position="bottom-right" :offset="[18, 18]">
+        <q-item-section side top>
+          <q-item-label caption>{{ expense.amount }} LE</q-item-label>
+        </q-item-section>
+        <q-separator spaced inset />
+      </q-item>
+
+    </q-list>
+    <!-- <h1>Home page</h1>
+    <p>You have: $ {{ totalAmount }}</p> -->
+    
+    <q-page-sticky position="bottom-right" :offset="[18, 18]">
       <q-btn fab icon="add" color="primary" :to="{ name: 'add-expense' }">
       </q-btn>
     </q-page-sticky>
+  </q-page>
 </template>
