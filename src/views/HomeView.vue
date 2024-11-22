18a18d6b709060b2ad26f1ed99fbdba073d50863
Moustafa Shaaban <Moustafa_Shaaban@zohomail.com>
2024-11-22 11:01:34 +0200

    Updated HomeView.vue to use Quasar Table

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { ref } from 'vue';
+import { date } from 'quasar'
+
 import { useTransactionsStore } from '@/stores/transactions';
 
 const transactionsStore = useTransactionsStore();
@@ -10,8 +13,30 @@ const transactions = transactionsStore.transactions;
 const expenses = transactionsStore.getExpenses;
 const incomes = transactionsStore.getIncomes;
 
+const title = expenses.value
+
 const totalIncomes = transactionsStore.totalIncomes;
 const totalExpenses = transactionsStore.totalExpenses;
+
+const filter = ref('');
+
+const columns = [
+  {
+    name: 'name',
+    label: "Name",
+    required: true,
+    align: 'left',
+    field: row => row.name,
+    format: val => `${val}`,
+    sortable: true
+  },
+  {name: 'Date', label: 'Date',field: row => date.formatDate(row.dateAdded, 'DD MMMM YYYY'), sortable: true},
+  { name: 'Amount',label: 'Amount', field: row => row.amount, sortable: true },
+  { name: 'Tags', label: 'Tags', field: row => row.tags.name },
+  { name: 'delete', label: 'Actions' },
+]
+
+const rows = transactions;
 
 function onClick() {
   // console.log('Clicked on a QChip')
@@ -19,8 +44,8 @@ function onClick() {
 </script>
 
 <template>
-  <q-page class="">
-    <q-card class="my-card bg-primary text-white">
+  <q-page class="q-pa-md">
+    <!-- <q-card class="my-card bg-primary text-white">
       <q-card-section>
         <div class="text-h6">Your Expenses</div>
         <div class="text-subtitle2">Balance: {{ totalAmount }}</div>
@@ -43,6 +68,7 @@ function onClick() {
           <q-item-label>
             <div class="q-pa-md">
               {{ expense.name }}
+              <span>{{ date.formatDate(expense.dateAdded, 'DD MMMM YYYY') }}</span>
               <q-chip v-for="tag in expense.tags" :key="tag.id" size="sm" clickable @click="onClick" color="primary"
                 text-color="white" icon="event">
                 {{ tag.name }}
@@ -56,9 +82,34 @@ function onClick() {
         </q-item-section>
       </q-item>
 
-    </q-list>
+    </q-list> -->
     <!-- <h1>Home page</h1>
     <p>You have: $ {{ totalAmount }}</p> -->
+    <q-table
+      :grid="$q.screen.xs"
+      flat bordered
+      title="Transactions"
+      :rows="rows"
+      :columns="columns"
+      row-key="id"
+      :filter="filter"
+      
+    >
+    <template v-slot:bottom-left>
+      <p>You have: $ {{ totalAmount }}</p>
+        
+      </template>
+    
+      <template v-slot:top-right>
+        <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+        
+      </template>
+    </q-table>
+
 
     <q-page-sticky position="bottom-right" :offset="[18, 18]">
       <q-btn fab icon="add" color="primary" :to="{ name: 'add-expense' }">
