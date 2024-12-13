c7c48203a3a95db3f8c0ee01ebcb4d63c312ff5f
Moustafa Shaaban <57029110+MoustafaShaaban@users.noreply.github.com>
2024-12-13 14:52:18 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     // @quasar/plugin-vite options list:
     // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar({
-      sassVariables: 'src/quasar-variables.sass'
+      
     })
   ],
 })
