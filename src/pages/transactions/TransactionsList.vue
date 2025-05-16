3572492de88d6ab5348951da85bf55ea5713b9d9
Moustafa Shaaban <Moustafa_Shaaban@zohomail.com>
2025-05-16 20:43:59 +0300

    Removed unneccesary comments

--- src/pages/transactions/TransactionsList.vue
+++ src/pages/transactions/TransactionsList.vue
@@ -36,9 +36,9 @@ function confirm(id) {
     }
   })
 };
-// let groupedTransactions = transactionsStore.groups;
+
 const filter = ref('');
-const columns = [
+const columns = ref([
   {
     name: 'name',
     label: "Name",
@@ -50,11 +50,12 @@ const columns = [
   },
   { name: 'Date', label: 'Date', field: row => date.formatDate(row.dateAdded, 'DD MMMM YYYY'), sortable: true },
   { name: 'Amount', label: 'Amount', field: row => row.amount, sortable: true },
-  //{ name: 'Tags', label: 'Tags', field: row => row.tags },
-]
+])
 const transactions = computed(() => {
   return transactionsStore.transactions
 })
+
+
 const rows = transactions
 </script>
 
@@ -82,14 +83,6 @@ const rows = transactions
                 <div class="col">
                   <div class="text-h6">{{ props.row.name }}</div>
                   <div class="text-subtitle2">{{ date.formatDate(props.row.date, "YYYY-MM-DD") }}</div>
-                  <!-- <q-item-label caption>
-                    <div>
-                      <q-badge side clickable rounded color="primary" class="q-mx-xs" v-for="tag in props.row.tags"
-                        :key="tag.id">
-                        <q-breadcrumbs-el :label="tag.name" :to="{ name: 'tag-details', params: { id: tag.id } }" />
-                      </q-badge>
-                    </div>
-                  </q-item-label> -->
                 </div>
                 <div class="col-auto">
                   <q-badge side clickable rounded class="q-mx-xs" v-for="tag in props.row.tags" :key="tag.id"
@@ -129,74 +122,6 @@ const rows = transactions
         </div>
       </template>
     </q-table>
-
-    <!-- <q-list v-if="transactionsStore.transactions.length">
-      <q-item v-for="transaction in transactionsStore.transactions" :key="transaction.id">
-        <q-item-section top class="col-2 gt-sm">
-          <q-item-label class="q-mt-sm">{{ transaction.name }}</q-item-label>
-        </q-item-section>
-
-        <q-item-section top>
-
-          <q-item-label lines="1">
-            <span class="text-weight-medium">{{ date.formatDate(transaction.dateAdded, 'DD MMMM YYYY') }}</span>
-          </q-item-label>
-          <q-item-label caption lines="1">
-            <div>
-              <q-badge clickable rounded color="primary" class="q-mx-xs" v-for="tag in transaction.tags" :key="tag.id">
-                <q-breadcrumbs-el :label="tag.name" :to="{ name: 'tag-details', params: { id: tag.id } }" />
-              </q-badge>
-            </div>
-          </q-item-label>
-          <q-item-label lines="1" class="q-mt-xs text-body2 text-weight-bold text-primary">
-            <span class="cursor-pointer">{{ transaction.amount }} $</span>
-          </q-item-label>
-        </q-item-section>
-
-        <q-item-section top side>
-          <div class="text-grey-8 q-gutter-xs">
-            <q-btn clickable :to="{ name: 'transaction-details', params: { id: transaction.id } }" class="gt-xs"
-              size="12px" flat dense round icon="edit" />
-            <q-btn class="gt-xs" size="12px" flat dense round icon="delete" @click="confirm(transaction.id)" />
-
-            <q-btn size="12px" flat dense round icon="more_vert" />
-          </div>
-        </q-item-section>
-      </q-item>
-    </q-list>
-    <p v-else>
-      No Transactions Found. Click on the plus sign to add a new one
-    </p>
-  -->
-    <!-- <q-table :grid="$q.screen.xs" flat bordered title="Transactions" :rows="rows" :columns="columns" row-key="id"
-      :filter="filter">
-      <template v-slot:bottom-left>
-        <p>You have: $ {{ totalAmount }}</p>
-      </template>
-
-<template v-slot:top-right>
-        <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
-          <template v-slot:append>
-            <q-icon name="search" />
-          </template>
-</q-input>
-
-</template>
-</q-table> -->
-
-    <!-- <div>
-      <q-list bordered class="rounded-borders">
-        <q-expansion-item v-for="group in groupedTransactions" expand-separator icon="perm_identity"
-          label="Account settings" caption="John Doe">
-          <q-card v-for="item in group" :key="item.id">
-            <q-card-section>
-              {{ date.formatDate(item.dateAdded, 'DD MMMM YYYY') }}
-            </q-card-section>
-          </q-card>
-        </q-expansion-item>
-      </q-list>
-    </div> -->
-
 
     <q-page-sticky position="bottom-right" :offset="[18, 18]">
       <q-btn fab icon="add" color="primary" :to="{ name: 'create' }">
